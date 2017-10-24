3d234924af0991b147ecfee71520c6d05572d3e5
Leo Leung <leungleoqin@gmail.com>
2017-10-24 14:49:03 -0700

    Implement barebone express server

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT);
+
+/* LOGGING */
+
+console.log(`Server listening on port ${PORT}`);
+app.use((req, res, next) => {
+  console.log(`${req.method} request on ${req.url}`);
+  next();
+});
+
+/* REQUEST HANDLERS */
+
+const getAllProductsHandler = (req, res) => {
+  res.send('get all products');
+};
+
+const getProductHandler = (req, res) => {
+  res.send('get one product');
+};
+
+const postProductHandler = (req, res) => {
+  res.send('post product');
+};
+
+const putProductHandler = (req, res) => {
+  res.send('updat product');
+};
+
+const deleteProductHandler = (req, res) => {
+  res.send('delete product');
+};
+
+/* PATHS */
+
+app.get('/products', getAllProductsHandler);
+app.get('/products/:productId', getProductHandler);
+app.post('/products', postProductHandler);
+app.put('/products/:productId', putProductHandler);
+app.delete('/products/:productId', deleteProductHandler);
