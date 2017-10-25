d8c2ea84ccd6f642f47f573cb49b4b28e02b002c
Leo Leung <leungleoqin@gmail.com>
2017-10-24 22:03:01 -0700

    Fix merging errors

--- db/schema.sql
+++ db/schema.sql
@@ -2,13 +2,13 @@
 -- CREATE DATABASE inventory;
 
 CREATE TABLE IF NOT EXISTS category (
-  cat_id serial PRIMARY KEY,
+  id serial PRIMARY KEY,
   name varchar(40) NOT NULL UNIQUE,
-  parent_cat_id integer references category(cat_id)
+  parent_cat_id integer references category(id)
 );
 
 CREATE TABLE IF NOT EXISTS product (
-  id integer PRIMARY KEY,
+  id serial PRIMARY KEY,
   name varchar(80) NOT NULL,
   description text NOT NULL,
   standard_price money NOT NULL,
@@ -16,14 +16,8 @@ CREATE TABLE IF NOT EXISTS product (
   cat_id integer NOT NULL references category(id)
 );
 
-CREATE TABLE IF NOT EXISTS category (
-  id integer PRIMARY KEY,
-  name varchar(40) NOT NULL,
-  parent_cat_id integer references category(id)
-);
-
 CREATE TABLE IF NOT EXISTS product_img (
-  id integer PRIMARY KEY,
+  id serial PRIMARY KEY,
   product_id integer NOT NULL references product(id),
   img_url varchar(200) NOT NULL,
   primary_img boolean NOT NULL
