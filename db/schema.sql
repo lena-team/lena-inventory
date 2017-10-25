66f99995322711c4b0e7c645075495fa2dd42a0a
Leo Leung <leungleoqin@gmail.com>
2017-10-24 18:58:35 -0700

    Modify schema to simplify query functions

--- db/schema.sql
+++ db/schema.sql
@@ -8,17 +8,23 @@ CREATE TABLE IF NOT EXISTS category (
 );
 
 CREATE TABLE IF NOT EXISTS product (
-  product_id serial PRIMARY KEY,
+  id integer PRIMARY KEY,
   name varchar(80) NOT NULL,
   description text NOT NULL,
   standard_price money NOT NULL,
   discounted_price money,
-  cat_id integer NOT NULL references category(cat_id)
+  cat_id integer NOT NULL references category(id)
+);
+
+CREATE TABLE IF NOT EXISTS category (
+  id integer PRIMARY KEY,
+  name varchar(40) NOT NULL,
+  parent_cat_id integer references category(id)
 );
 
 CREATE TABLE IF NOT EXISTS product_img (
-  img_id serial PRIMARY KEY,
-  product_id integer NOT NULL references product(product_id),
+  id integer PRIMARY KEY,
+  product_id integer NOT NULL references product(id),
   img_url varchar(200) NOT NULL,
   primary_img boolean NOT NULL
 );
